8a88b7be433678c99371027272c4f65ccb0bcc06
Harshit <harshit2001pandey@gmail.com>
2024-07-13 23:34:43 +0530

    3 Design Update after KeyDown event

--- src/Componets/FoodItem.jsx
+++ src/Componets/FoodItem.jsx
@@ -1,7 +1,7 @@
 import styles from './FoodItem.module.css'
 function FoodItem({item}){
     return (
-        <li  className="list-group-item">{item} 
+        <li  className={`${styles.listItems} list-group-item`}>{item} 
                 <button className={`${styles.button} btn btn-secondary`}>Add</button>
         </li>
     );
